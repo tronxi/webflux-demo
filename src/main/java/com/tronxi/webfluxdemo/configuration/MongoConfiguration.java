efeee8371d0891c5c1ee976510101bca64c1b832
tronxi <sergio.gsanchez97@gmail.com>
2020-09-06 22:45:34 +0200

    added delay insert

--- src/main/java/com/tronxi/webfluxdemo/configuration/MongoConfiguration.java
+++ src/main/java/com/tronxi/webfluxdemo/configuration/MongoConfiguration.java
@@ -5,6 +5,11 @@ import com.tronxi.webfluxdemo.repository.document.DemoDocument;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -13,8 +18,13 @@ public class MongoConfiguration {
     private final DemoRepository demoRepository;
     @Bean
     public void setData() {
+        List<DemoDocument> demoDocumentList = new ArrayList<>();
         for(int i = 0; i < 10; i++) {
-            demoRepository.insert(new DemoDocument(String.valueOf(i))).subscribe();
+            demoDocumentList.add(new DemoDocument(String.valueOf(i)));
         }
+
+        Flux.fromIterable(demoDocumentList)
+                .delayElements(Duration.ofMillis(100))
+                .subscribe(dc -> demoRepository.insert(dc).subscribe());
     }
 }
